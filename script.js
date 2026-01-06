236cfb5be6df02e9236d6a389a68ddf84070fec2
inioluwa <ajayiinioluwa2007@gmail.com>
2026-01-06 03:25:13 -0800

    created my dashboard interface and landing page interface

---
+++ script.js
@@ -0,0 +1,14 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDBPPeJaDIpQ3l6MgGHcrAYgUXRvRmEJPU",
+    authDomain: "subminder-trials.firebaseapp.com",
+    databaseURL: "https://subminder-trials-default-rtdb.firebaseio.com",
+    projectId: "subminder-trials",
+    storageBucket: "subminder-trials.firebasestorage.app",
+    messagingSenderId: "967844106132",
+    appId: "1:967844106132:web:305b6a876595266a198981"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
